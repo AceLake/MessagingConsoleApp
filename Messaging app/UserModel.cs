2eb61853d5af0b4a52306875137f24eabc7b2207
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-05-02 12:13:47 -0600

    Set up the UserModel

--- Messaging app/UserModel.cs
+++ Messaging app/UserModel.cs
@@ -11,6 +11,11 @@ namespace Messaging_app
         public int ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-
+        public string ProfilePicture { get; set; }
+        public bool Status { get; set; }
+        public List<UserModel> Contacts { get; set; }
+        public string Preferences { get; set; }
+        public List<MessageModel> History { get; set; }
+        public List<ConversationModel> Groups { get; set; }
     }
 }
