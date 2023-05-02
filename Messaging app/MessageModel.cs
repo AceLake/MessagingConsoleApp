300d88bff1401cfae9b5fc467f67c3f53da2f640
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-05-02 12:05:36 -0600

    Created a MessageModel

--- Messaging app/MessageModel.cs
+++ Messaging app/MessageModel.cs
@@ -8,5 +8,10 @@ namespace Messaging_app
 {
     internal class MessageModel
     {
+        public int ID { get; set; }
+        public int SenderID { get; set; }
+        public int ConversationID { get; set; }
+        public string? Content { get; set; }
+        public DateTime TimeStammp { get; set; }
     }
 }
